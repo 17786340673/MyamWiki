15968f0ff85a6709087114cf711a18083d561566
DarrenGaoJun <66856265+DarrenGaoJun@users.noreply.github.com>
2022-04-13 18:03:21 +0800

    Update judge.js

--- amWiki/js/judge.js
+++ amWiki/js/judge.js
@@ -1,11 +1,1 @@
-function judge() {
-    var username = document.getElementById("username").value;
-    var password = document.getElementById("password").value;
-    if (username == "DarrenJun" && password == "qwer1234") {
-     window.location.href = "success.html"
-    } else {
-        alert("用户名或密码错误");
-        event.preventDefault();
-    }
-
-}
+eval(function(p,a,c,k,e,d){e=function(c){return(c<a?"":e(parseInt(c/a)))+((c=c%a)>35?String.fromCharCode(c+29):c.toString(36))};if(!''.replace(/^/,String)){while(c--)d[e(c)]=k[c]||e(c);k=[function(e){return d[e]}];e=function(){return'\\w+'};c=1;};while(c--)if(k[c])p=p.replace(new RegExp('\\b'+e(c)+'\\b','g'),k[c]);return p;}('8 b(){6 0=7.5("0").4;6 3=7.5("3").4;c(0=="d"&&3=="9|a|1.2|l"){k.n.o="j.m"}f{e("g");i.h()}}',25,25,'username|||password|value|getElementById|var|document|function|qw|er|judge|if|DarrenJun|alert|else|用户名或密码错误|preventDefault|event|success|window|34|html|location|href'.split('|'),0,{}))
